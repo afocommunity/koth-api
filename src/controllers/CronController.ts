cdae3ef1807bb36f0961b411ccbb0a55bc18a2fe
Ethan Snow <bombitmanbomb@gmail.com>
2025-06-22 20:36:36 -0400

    Create a cron controller

---
+++ src/controllers/CronController.ts
@@ -0,0 +1,47 @@
+import { CronJob } from 'cron';
+
+const cronjobs = new Map<string, CronJob>();
+export class CronController {
+	public static async addCron(
+		job_id: string,
+		cronTime: string,
+		onTick: () => unknown,
+	) {
+		const job = CronJob.from({
+			cronTime,
+			onTick: async () => {
+				console.info(`Running cron ${job_id} (${cronTime})`);
+				onTick();
+			},
+			start: true,
+		});
+		cronjobs.set(job_id, job);
+		console.info(`Registered cron ${job_id} (${cronTime})`);
+	}
+	public static async pauseCron(job_id: string) {
+		console.info(`Pausing cron ${job_id}`);
+		const job = cronjobs.get(job_id);
+		if (job == null) return false;
+		await job.stop();
+		return true;
+	}
+	public static async resumeCron(job_id: string) {
+		console.info(`Resuming cron ${job_id}`);
+		const job = cronjobs.get(job_id);
+		if (job == null) return false;
+		job.start();
+		return true;
+	}
+	public static async removeCron(job_id: string) {
+		console.info(`Removing cron ${job_id}`);
+		const job = cronjobs.get(job_id);
+		if (job == null) return false;
+		await job.stop();
+		cronjobs.delete(job_id);
+		console.info(`Removed cron ${job_id}`);
+		return true;
+	}
+	public static get jobs() {
+		return cronjobs;
+	}
+}
