c6dde80b9910a7385a090bbfebf7bd3a8dd85aca
Ethan Snow <bombitmanbomb@gmail.com>
2025-07-14 01:36:04 -0400

    squadjs events

---
+++ src/squadjs/events/KOTH-update-tickets.js
@@ -0,0 +1,15 @@
+export default {
+	regex:
+		/^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquad: \[SQLogOutputAndMessage\] BP_Ruleset_KOTH_C_\d* KOTH NOTIFY: Points Updated: blufor: (\d*) redfor: (\d*) indfor: (\d*)/,
+	onMatch: (args, logParser) => {
+		const data = {
+			raw: args[0],
+			time: args[1],
+			chainID: args[2],
+      blufor: args[3],
+      redfor: args[4],
+      indfor: args[5]
+		};
+		logParser.eventStore.KOTH_TICKETS = data;
+	},
+};
