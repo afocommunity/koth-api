e10828d73d7ecfd053736a8f6fc3a8e94d2fbadc
Ethan Snow <bombitmanbomb@gmail.com>
2025-10-03 13:50:19 -0400

    feat: swagger

--- src/services/plugin-file/plugin-file.controller.ts
+++ src/services/plugin-file/plugin-file.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { buildPlugin } from './squadjs/buildPluginFile';
 import { CURRENT_PLUGIN_VERSION } from './squadjs/CURRENT_PLUGIN_VERSION';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('plugin-file')
 export class PluginFileController {
+	@ApiOperation({ summary: 'Get the latest plugin file' })
+	@ApiOkResponse({ description: 'Latest plugin file' })
 	@Get('/download')
+	@Header('content-type', 'text/javascript')
 	public async getPlugin() {
 		return buildPlugin();
 	}
+
+	@ApiOperation({ summary: 'Get the latest plugin version' })
+	@ApiOkResponse({ description: 'Latest plugin version' })
 	@Get('/version')
 	public async getPluginVersion() {
 		return CURRENT_PLUGIN_VERSION;
