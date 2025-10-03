c4274ad546e9df4667f789df2e615066af7e1fd7
Ethan Snow <bombitmanbomb@gmail.com>
2025-10-03 00:21:15 -0400

    feat: swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+
+	const config = new DocumentBuilder()
+		.setTitle('KOTH API')
+		.setDescription('API for interfacing with KapiSync')
+		.setVersion('1.0')
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api-docs', app, documentFactory, {
+		jsonDocumentUrl: 'swagger/json',
+	});
 	await app.listen(3131);
 }
 bootstrap();
