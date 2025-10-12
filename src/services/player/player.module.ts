afa36ab25f89c5401d348048f2281f7d52b2af05
Ethan Snow <bombitmanbomb@gmail.com>
2025-10-12 16:39:50 -0400

    fix dependency injection of Player

--- src/services/player/player.module.ts
+++ src/services/player/player.module.ts
@@ -3,11 +3,23 @@ import { PlayerController } from './player.controller';
 import { Player } from './player.model';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { PlayerService } from './player.service';
+import { LoadoutItem } from '@/models/loudout-item.model';
+import { PermaUnlocks } from '@/models/perma-unlock.model';
+import { PlayerSave } from '@/models/player-save.model';
+import { WeaponXP } from '@/models/weapon-xp.model';
 
 @Module({
 	controllers: [PlayerController],
 	providers: [PlayerService],
-	imports: [SequelizeModule.forFeature([Player])],
+	imports: [
+		SequelizeModule.forFeature([
+			Player,
+			LoadoutItem,
+			PermaUnlocks,
+			PlayerSave,
+			WeaponXP,
+		]),
+	],
 	exports: [SequelizeModule],
 })
 export class PlayerModule {}
